e354fb434abc98416b9b18abdee0b2d46811458b
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-03-05 22:41:14 -0300

    implementando entity post com user aninhado

---
+++ workshopmongo/src/main/java/com/eduardo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.eduardo.workshopmongo.repository;
+
+import com.eduardo.workshopmongo.domain.Post;
+import com.eduardo.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+
+}
