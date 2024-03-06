b397772a6dc34ef859a8d30ace87495c81c0077b
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-03-05 23:10:30 -0300

    obtendo post por id

---
+++ workshopmongo/src/main/java/com/eduardo/workshopmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.eduardo.workshopmongo.services;
+
+import com.eduardo.workshopmongo.domain.Post;
+import com.eduardo.workshopmongo.repository.PostRepository;
+import com.eduardo.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+    }
+}
