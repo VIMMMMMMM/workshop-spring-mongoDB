09c5647b25b006efbada9edd94ebda229d790e17
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-03-06 00:08:14 -0300

    implementando consulta com varios criterios

--- workshopmongo/src/main/java/com/eduardo/workshopmongo/resources/util/URL.java
+++ workshopmongo/src/main/java/com/eduardo/workshopmongo/resources/util/URL.java
@@ -3,9 +3,22 @@ package com.eduardo.workshopmongo.resources.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text){
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+    public static Date convertDate(String textDate, Date defaultDate){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return simpleDateFormat.parse(textDate);
+        } catch (ParseException e) {
+            return defaultDate;
+        }
     }
 }
