607b014bcfd069324438c188b40c248d3eb1d829
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-03-05 22:47:13 -0300

    implementando padrao DTO para post

--- workshopmongo/src/main/java/com/eduardo/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/eduardo/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.eduardo.workshopmongo.config;
 
 import com.eduardo.workshopmongo.domain.Post;
 import com.eduardo.workshopmongo.domain.User;
+import com.eduardo.workshopmongo.dto.AuthorDTO;
 import com.eduardo.workshopmongo.repository.PostRepository;
 import com.eduardo.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +30,12 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        Post post1 = new Post(null,simpleDateFormat.parse("05/03/2024"),"adios","indo viajar",maria);
-        Post post2 = new Post(null,simpleDateFormat.parse("07/03/2024"),"bao","acordei",maria);
-
 
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        Post post1 = new Post(null,simpleDateFormat.parse("05/03/2024"),"adios","indo viajar",new AuthorDTO(maria));
+        Post post2 = new Post(null,simpleDateFormat.parse("07/03/2024"),"bao","acordei",new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
